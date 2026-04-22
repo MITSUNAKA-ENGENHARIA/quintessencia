148a6d22acbbb91766ee2cd20c9df51971973c70
kevindotklein <kevindotklein@gmail.com>
2026-04-22 19:44:14 -0300

    feat: add keyboard interrupt

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from neonize.client import NewClient
-from neonize.events import MessageEv, ConnectedEv, event
+from neonize.events import MessageEv, ConnectedEv
+
+import time
+import threading
+import sys
 
 client = NewClient("quintessencia")
 
 @client.event(ConnectedEv)
 def on_connected(client: NewClient, event: ConnectedEv):
-    print("✅ Bot connected successfully!")
-    print(f"📱 Logged in as: {event.device.User}")
+    print("bot connected successfully!")
 
 @client.event(MessageEv)
 def on_message(client: NewClient, event: MessageEv):
@@ -19,16 +22,19 @@ def on_message(client: NewClient, event: MessageEv):
         client.send_message(sender, "pong porra")
         client.send_message(sender, sender.User)
         client.send_message(sender, name)
-        print(f"sender completo: {sender}")
-        print(f"sender.User: {sender.User}")
-        print(f"sender raw: {repr(sender)}")
+        print(f"sender ({sender.User}): {text}")
     elif text == "hello":
         client.reply_message("Hello! 👋 How can I help you?", event)
 
 def main():
-    client.connect()
-    event.wait()
-
+    t = threading.Thread(target=client.connect, daemon=True)
+    t.start()
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("shutting bot down...")
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
